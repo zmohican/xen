6c8449c5f00ba3a1dca7f770b5cfb3df6527786c
zmohican <zhouzhouxuxu@gmail.com>
2023-09-04 00:42:13 +0000

    1. update path

--- xen/qemu/qemu32_openwrt.sh
+++ xen/qemu/qemu32_openwrt.sh
@@ -3,9 +3,9 @@
 qemu-system-arm -machine orangepi-pc -nographic \
 	-device loader,file=/home/zhoux/git/linux/.arm32/arch/arm/boot/zImage,addr=0x50000000 \
 	-device loader,file=/home/zhoux/Downloads/openwrt-22.03.5-armvirt-32-rootfs.cpio,addr=0x52000000 \
-	-device loader,file=domU1.dtb,addr=0x53000000 \
-	-sd openwrt-22.03.5-sunxi-cortexa7-xunlong_orangepi-pc-ext4-sdcard.img \
-	-kernel ../.arm32/xen  
+	-device loader,file=qemu/domU1.dtb,addr=0x53000000 \
+	-sd qemu/openwrt-22.03.5-sunxi-cortexa7-xunlong_orangepi-pc-ext4-sdcard.img \
+	-kernel .arm32/xen  
 
 #	-device loader,file=/home/zhoux/Downloads/openwrt-22.03.5-armvirt-32-zImage,addr=0x50000000 \
 #	-netdev user,id=mynet0 -device virtio-net-device,netdev=mynet0,bus=virtio-mmio-bus.0 
